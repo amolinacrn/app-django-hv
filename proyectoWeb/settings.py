206ff7f924e2c9884c21aac2601628d54c4170ee
Alejandro Molina <malejo20@gmail.com>
2026-02-08 23:19:01 -0500

    Update settings.py

--- proyectoWeb/settings.py
+++ proyectoWeb/settings.py
@@ -179,10 +179,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # MEDIA_URL = '/media/'
 
 
-#MEDIA_ROOT = BASE_DIR / "media"
-#MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "media/"
 
-MEDIA_URL = "https://tkkhlfkmsbbbqmdsbhug.storage.supabase.co/storage/v1/object/public/media/"
+#MEDIA_URL = "https://tkkhlfkmsbbbqmdsbhug.storage.supabase.co/storage/v1/object/public/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -206,6 +206,7 @@ LATEX_INTERPRETER = "pdflatex"
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
 XS_SHARING_ALLOWED_METHODS = ["POST", "GET", "OPTIONS", "PUT", "DELETE"]
+
 
 
 
